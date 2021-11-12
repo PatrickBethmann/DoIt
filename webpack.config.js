f926904693639a750499b2629028a67d6be12aaa
Patrick Bethmann <patrickbethmann@outlook.de>
2021-11-12 13:26:37 +0100

    fixed title issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: "./src/index.js",
-    },
-    devtool: "inline-source-map",
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "ToDo-List",
-            template: "src/index.html",
-        }),
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'ToDo-List',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        type: 'asset/resource',
+      },
     ],
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpg)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
+  },
 };
